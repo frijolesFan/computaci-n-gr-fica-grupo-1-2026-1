bd61e7943514be392038ec74f6420c8332784d82
frijolesFan <juan.guevara3@utp.edu.co>
2026-03-06 15:49:10 -0500

    progreso de clase

--- taller8/taller8.py
+++ taller8/taller8.py
@@ -3,7 +3,21 @@ import matplotlib.pyplot as plt
 import ImgProcess as imp
 
 def punto1():
-    pass
+    lienzo = np.zeros((3, 3, 3), dtype=np.uint8)
+    lienzo[0, 0, :] = [0, 255, 255]  #cian
+    lienzo[0, 1, :] = [255, 255, 255]  #blanco
+    lienzo[0, 2, :] = [255, 0, 0]  #rojo
+    
+    lienzo[1, 0, :] = [255, 0, 255]  #magenta
+    lienzo[1, 1, :] = [255/2, 255/2, 255/2]  #gris
+    lienzo[1, 2, :] = [0, 255, 0]  #verde
+
+    lienzo[2, 0, :] = [255, 255, 0]  #amarillo
+    lienzo[2, 1, :] = [0, 0, 0]  #negro
+    lienzo[2, 2, :] = [0, 0, 255]  #azul
+
+    plt.imshow(lienzo)
+    plt.show()
 
 def punto3():
     imagen = plt.imread('resources/dawg.jpg')
@@ -36,4 +50,4 @@ def punto7():
     plt.show()
 
 if __name__ == "__main__":
-    punto7()
+    punto1()
