ab931f68c895d2bdfd3dac5c4c0098dfc22e2acc
frijolesFan <juan.guevara3@utp.edu.co>
2026-03-06 15:36:49 -0500

    avances al final de clase

--- taller8/ImgProcess.py
+++ taller8/ImgProcess.py
@@ -4,3 +4,22 @@ import matplotlib.pyplot as plt
 def negative(img):
     return 255 - img
 
+def red_channel(img):
+    imgCopy = img.copy()
+    imgCopy[:, :, 1] = imgCopy[:, :, 2] = 0
+    return imgCopy
+
+def green_channel(img):
+    imgCopy = img.copy()
+    imgCopy[:, :, 0] = imgCopy[:, :, 2] = 0
+    return imgCopy
+
+def blue_channel(img):
+    imgCopy = img.copy()
+    imgCopy[:, :, 0] = imgCopy[:, :, 1] = 0
+    return imgCopy
+
+def magenta(img):
+    imgCopy = img.copy()
+    imgCopy[:, :, 1] = 0
+    return imgCopy
--- taller8/taller8.py
+++ taller8/taller8.py
@@ -11,5 +11,29 @@ def punto3():
     plt.imshow(imagenNega)
     plt.show()
 
+def punto4():
+    imagen = plt.imread('resources/dawg.jpg')
+    imagenRed = imp.red_channel(imagen)
+    plt.imshow(imagenRed)
+    plt.show()
+
+def punto5():
+    imagen = plt.imread('resources/dawg.jpg')
+    imagenGreen = imp.green_channel(imagen)
+    plt.imshow(imagenGreen)
+    plt.show()
+
+def punto6():
+    imagen = plt.imread('resources/dawg.jpg')
+    imagenBlue = imp.blue_channel(imagen)
+    plt.imshow(imagenBlue)
+    plt.show()
+
+def punto7():
+    imagen = plt.imread('resources/dawg.jpg')
+    imagenMagenta = imp.magenta(imagen)
+    plt.imshow(imagenMagenta)
+    plt.show()
+
 if __name__ == "__main__":
-    punto3()
+    punto7()
